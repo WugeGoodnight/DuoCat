e6860c74a60c80b4db978d634605f5e4bc1d6dd1
wuhao <wuhao@xueqiu.com>
2015-12-21 16:39:07 +0800

    add origin files

---
+++ DuoCat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DuoCat
+//
+//  Created by 吴昊 on 15/12/21.
+//  Copyright © 2015年 wuge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DuoCat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DuoCat
+//
+//  Created by 吴昊 on 15/12/21.
+//  Copyright © 2015年 wuge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
